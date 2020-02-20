b77886b4c053813930a62a0fc62a4d24de36fe9c
Erin DeLaney <erinkdelaney@gmail.com>
2020-02-20 01:44:38 -0800

    Update Circles4.ino

--- Programs/Circles4/Circles4.ino
+++ Programs/Circles4/Circles4.ino
@@ -16,6 +16,8 @@ int num_circles = 3;
 int start_index [] = {1, 70, 170};
 int origin_index [] = {51, 85, 186};
 
+int color_state[NUM_LIGHTS] = {};
+
 // TO DO
 // 1. switch to using arrays in direction functions
 // 2. use global vars for start and origin indicies
@@ -30,38 +32,41 @@ void setup() {
 }
 
 
-int forwardCircleOrigin(int s , int i){
-    if (i + origin_index[s] > start_index[s] + 68){
-       return i + origin_index[s] - 68;
-      } else {
-        return i + origin_index[s];
-      }
-}
 
-int reverseCircleOrigin(int s , int i){
-    if (origin_index[s] - i < start_index[s]){
-       return origin_index[s] - i + 68;
-    } else {
-      return origin_index[s] - i;
-    }
-}
+//int forwardCircleOrigin(int s , int i){
+//    if (i + origin_index[s] > start_index[s] + 68){
+//       return i + origin_index[s] - 68;
+//      } else {
+//        return i + origin_index[s];
+//      }
+//}
+//
+//int reverseCircleOrigin(int s , int i){
+//    if (origin_index[s] - i < start_index[s]){
+//       return origin_index[s] - i + 68;
+//    } else {
+//      return origin_index[s] - i;
+//    }
+//}
 
 
 
 
 void loop() {
-//  uint32_t random = rand() % 3;
-  for (int i = 0; i < 35; i++) {
-    for (int a = 0; a < num_circles; a++) {
-        strip.setPixelColor(forwardCircleOrigin(a, i), SKY);  
-        strip.setPixelColor(reverseCircleOrigin(a, i), BERRY); 
-    }
-      strip.show();
-        delay(50); 
-    for (int b = 0; b < circles; b++) {
-        strip.setPixelColor(forwardCircleOrigin(b, i), BLACK);
-        strip.setPixelColor(reverseCircleOrigin(b, i), BLACK); 
-    }
-  }
+  
+  for (int i = 0; i < NUM_LIGHTS; i++) {
+     if (i > 50) {
+       color_state[i] = BERRY;
+     } else {
+       color_state[i] = SKY;
+     }
+   }
 
+    for (int a = 0; a < NUM_LIGHTS; a++) {
+      strip.setPixelColor(a, color_state[a]);
+    }
+    
+  strip.show();
+  delay(50); 
+  
 }
